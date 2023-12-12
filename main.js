eaea59e95b2bead8617ce0704ff0bc5bafee1150
42MacAntoine <amontalb@student.42lyon.fr>
2023-12-12 12:54:18 +0100

    adding a trail to the ball

--- main.js
+++ main.js
@@ -1,128 +1,148 @@
-const pongCanva = document.getElementById('pongCanvas');
-const context = pongCanva.getContext('2d');
+const pongCanvas = document.getElementById('pongCanvas');
+const scoreCanvas = document.getElementById('scoreCanvas');
+const context = pongCanvas.getContext('2d');
+const scoreContext = scoreCanvas.getContext('2d');
 
 // Paddle and ball properties
 const paddleWidth = 10;
-const paddleHeight = 100;
-let leftPaddleY = pongCanva.height / 2 - paddleHeight / 2;
-let rightPaddleY = pongCanva.height / 2 - paddleHeight / 2;
+const paddleHeight = 125;
+let leftPaddleY = pongCanvas.height / 2 - paddleHeight / 2;
+let rightPaddleY = pongCanvas.height / 2 - paddleHeight / 2;
 const ballSize = 10;
-let ballX = pongCanva.width / 2;
-let ballY = pongCanva.height / 2;
+let ballX = pongCanvas.width / 2;
+let ballY = pongCanvas.height / 2;
 let ballSpeedX = 5;
 let ballSpeedY = 5;
 
-// score
+// Score
 let leftPlayerScore = 0;
 let rightPlayerScore = 0;
 
+// Array to store previous positions and colors of the ball for the trail effect
+const ballTrail = [];
 
-function draw() 
-{
-	var score = scoreCanvas.getContext('2d'); 
-	var context = pongCanva.getContext('2d');
-
-	score.clearRect(0, 0, scoreCanvas.width, scoreCanvas.height);
-
-	// Draw left player score
-    score.fillStyle = 'white';
-    score.font = '30px Arial';
-    score.fillText(leftPlayerScore, scoreCanvas.width / 4,scoreCanvas.height / 2);
-
-    // Draw right player score
-    score.fillText(rightPlayerScore, 3 * scoreCanvas.width / 4, scoreCanvas.height / 2);
-
-	// Draw field
-	context.fillStyle = 'black';
-	context.fillRect(0, 0, pongCanva.width, pongCanva.height);	
-	// Draw middle line
-	context.strokeStyle = 'white';
-	context.lineWidth = 5;	
-	context.beginPath();
-	context.moveTo(pongCanva.width / 2, 0);
-	context.lineTo(pongCanva.width / 2, pongCanva.height);
-	context.stroke();
-	context.lineWidth = 1;	
-
-	// Draw players
-	context.fillStyle = 'white';
-	context.fillRect(0, leftPaddleY, paddleWidth, paddleHeight);
-	context.fillRect(pongCanva.width - paddleWidth, rightPaddleY, paddleWidth, paddleHeight);	
-
-	// Draw ball
-	context.beginPath();
-	context.fillStyle = 'red';
-	context.arc(ballX, ballY, ballSize, 0, Math.PI * 2);
-	context.fill();	
-
-	// Move the ball
-	ballX += ballSpeedX;
-	ballY += ballSpeedY;	
-
-	// Bounce off top and bottom walls
-	if (ballY - ballSize < 0 || ballY + ballSize > pongCanva.height) {
-	    ballSpeedY = -ballSpeedY;
-	}	
-
-	// Bounce off paddles
-	if (
-	    (ballX - ballSize < paddleWidth && ballY > leftPaddleY && ballY < leftPaddleY + paddleHeight) ||
-	    (ballX + ballSize > pongCanva.width - paddleWidth && ballY > rightPaddleY && ballY < rightPaddleY + paddleHeight)
-	) {
-	    ballSpeedX = -ballSpeedX;
-	}	
-
-	// Move right paddle based on the ball's position
-	// if (ballY > rightPaddleY + paddleHeight / 2) {
-	//     rightPaddleY += 5;
-	// } else {
-	//     rightPaddleY -= 5;
-	// }	
-
-	// Prevent paddles from going out of bounds
-	leftPaddleY = Math.max(0, Math.min(leftPaddleY, pongCanva.height - paddleHeight));
-	rightPaddleY = Math.max(0, Math.min(rightPaddleY, pongCanva.height - paddleHeight));
-	
-	//change the score
-	if (ballX <= 0)
-	{
-		rightPlayerScore ++;
-		ballX = pongCanva.width / 2;
-	    ballY = pongCanva.height / 2;
-	}
-
-	if (ballX >= pongCanva.width)
-	{
-		leftPlayerScore ++;
-		ballX = pongCanva.width / 2;
-	    ballY = pongCanva.height / 2;
-	}
-
+function clearCanvas(canvas) {
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
 }
 
+function drawField() {
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, pongCanvas.width, pongCanvas.height);
 
-function gameLoop() 
-{
-    draw();
+    context.strokeStyle = 'white';
+    context.lineWidth = 5;
+    context.beginPath();
+    context.moveTo(pongCanvas.width / 2, 0);
+    context.lineTo(pongCanvas.width / 2, pongCanvas.height);
+    context.stroke();
+    context.lineWidth = 1;
+}
+
+function drawPlayers() {
+    context.fillStyle = 'red';
+    context.fillRect(0, leftPaddleY, paddleWidth, paddleHeight);
+    context.fillRect(pongCanvas.width - paddleWidth, rightPaddleY, paddleWidth, paddleHeight);
+}
+
+function drawBall() {
+    for (let i = ballTrail.length - 1; i >= 0; i--) 
+	{
+		if (i%3 == 0)
+		{
+        const { x, y, color } = ballTrail[i];
+        context.beginPath();
+        context.fillStyle = color;
+        context.arc(x, y, ballSize, 0, Math.PI * 2);
+        context.fill();
+		}
+    }
+}
+
+function drawScores() {
+    scoreContext.clearRect(0, 0, scoreCanvas.width, scoreCanvas.height);
+    scoreContext.fillStyle = 'white';
+    scoreContext.font = '30px Arial';
+    scoreContext.fillText(leftPlayerScore, scoreCanvas.width / 4, scoreCanvas.height / 2);
+    scoreContext.fillText(rightPlayerScore, 3 * scoreCanvas.width / 4, scoreCanvas.height / 2);
+}
+
+function updateGame() {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+    if (ballY - ballSize < 0 || ballY + ballSize > pongCanvas.height) {
+        ballSpeedY = -ballSpeedY;
+    }
+
+    if (
+        (ballX - ballSize < paddleWidth && ballY > leftPaddleY && ballY < leftPaddleY + paddleHeight) ||
+        (ballX + ballSize > pongCanvas.width - paddleWidth && ballY > rightPaddleY && ballY < rightPaddleY + paddleHeight)
+    ) {
+        ballSpeedX = -ballSpeedX;
+    }
+
+    leftPaddleY = Math.max(0, Math.min(leftPaddleY, pongCanvas.height - paddleHeight));
+    rightPaddleY = Math.max(0, Math.min(rightPaddleY, pongCanvas.height - paddleHeight));
+
+    if (ballX <= 0) {
+        rightPlayerScore++;
+        resetBall();
+    }
+
+    if (ballX >= pongCanvas.width) {
+        leftPlayerScore++;
+        resetBall();
+    }
+
+    // Add the current position and a random color to the trail
+    const randomColor = getRandomColor();
+    ballTrail.push({ x: ballX, y: ballY, color: randomColor });
+
+    // Remove the oldest positions if the trail is too long
+    if (ballTrail.length > 30) {
+        ballTrail.shift();
+    }
+}
+
+function resetBall() {
+    ballX = pongCanvas.width / 2;
+    ballY = pongCanvas.height / 2;
+    // Clear the ball trail when the ball is reset
+    ballTrail.length = 0;
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function gameLoop() {
+    clearCanvas(pongCanvas);
+    clearCanvas(scoreCanvas);
+
+    drawField();
+    drawPlayers();
+    drawBall();
+    drawScores();
+
+    updateGame();
     requestAnimationFrame(gameLoop);
 }
 
-// Handle keyboard input
 document.addEventListener('keydown', function (event) {
     if (event.key === 'ArrowUp') {
-		rightPaddleY -= 40;
-    } 
-	else if (event.key === 'ArrowDown') {
-		rightPaddleY += 40;
+        rightPaddleY -= 20;
+    } else if (event.key === 'ArrowDown') {
+        rightPaddleY += 20;
+    } else if (event.key === 'w') {
+        leftPaddleY -= 20;
+    } else if (event.key === 's') {
+        leftPaddleY += 20;
     }
-	if (event.key === 'w') {
-		leftPaddleY -= 40;
-    } 
-	else if (event.key === 's') {
-		leftPaddleY += 40;
-    }
-	
 });
 
-// Start the game loop
 gameLoop();

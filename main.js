5cfb1b00289e7a047ccbfe0f26e9b602e79d3523
42MacAntoine <amontalb@student.42lyon.fr>
2024-01-09 11:45:13 +0100

    ok background

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ const scoreCanvas = document.getElementById('scoreCanvas');
 const context = pongCanvas.getContext('2d');
 const scoreContext = scoreCanvas.getContext('2d');
 
+window.addEventListener('resize', function() {
+	const minWidth = 600; // La largeur minimale souhaitée
+	const minHeight = 400; // La hauteur minimale souhaitée
+  
+	if (window.innerWidth < minWidth || window.innerHeight < minHeight) {
+	  // Restaurer la taille minimale si elle est redimensionnée en dessous
+	  window.resizeTo(minWidth, minHeight);
+	}
+  });
